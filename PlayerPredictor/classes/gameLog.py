98276c11a6ebddb3a11809456ce78620b3e2608a
agerber0902 <agerber0902@gmail.com>
2026-01-10 16:12:37 -0500

    Get the passing maxes and totals
    Use the same functionality as rushing and receiving

--- PlayerPredictor/classes/gameLog.py
+++ PlayerPredictor/classes/gameLog.py
@@ -14,18 +14,26 @@ from .game import Game
 from utils import isValidType
 from typing import List, Dict
 
+MAX_PASSING_STAT_NAMES = ["pass_att", "pass_cmp", "pass_yds", "pass_td", "pass_int", "pass_long"]
 MAX_RUSHING_STAT_NAMES = ["rush_att", "rush_yds", "rush_td", "rush_long"]
 MAX_RECEIVING_STAT_NAMES = ["targets", "rec", "rec_yds", "rec_yds_per_rec", "rec_td", "rec_long"]
 MAX_STAT_NAMES = ["yds_per_touch", "yds_from_scrimage", "rush_receive_td", "fumbles"]
 
+TOTAL_PASSING_STAT_NAMES = ["pass_att", "pass_cmp", "pass_yds", "pass_td", "pass_int"]
 TOTAL_RUSHING_STAT_NAMES = ["rush_att", "rush_yds", "rush_td"]
 TOTAL_RECEIVING_STAT_NAMES = ["targets", "rec", "rec_yds", "rec_td"]
 
 @dataclass
 class GameLog:
     games: list[Game] = field(default_factory=list)
+    
+    #   Totals
+    passingTotals: Dict[str, int] = field(default_factory=dict)
     rushingTotals: Dict[str, int] = field(default_factory=dict)
-    receivingTotals: Dict[str, int] = field(default_factory=dict)    
+    receivingTotals: Dict[str, int] = field(default_factory=dict) 
+    
+    #   Maxes
+    passingMaxes: List[Dict[str, object]] = field(default_factory=list)
     rushingMaxes: List[Dict[str, object]] = field(default_factory=list)
     receivingMaxes: List[Dict[str, object]] = field(default_factory=list)
     
@@ -34,11 +42,28 @@ class GameLog:
         #   Set Totals
         self.set_rush_totals()
         self.set_rec_totals()
+        self.set_pass_totals()
         
         #   Set Max values
+        self.set_pass_maxes()
         self.set_rush_maxes()
         self.set_rec_maxes()          
         
+    #   Compute maxes for all rush stats
+    def set_pass_maxes(self):
+        for stat_name in MAX_PASSING_STAT_NAMES:
+            max_games = self.stat_max(stat_name)
+    
+            if not max_games:
+                continue
+    
+            max_value = getattr(max_games[0], stat_name, 0) or 0
+            game_dates = [g.date for g in max_games]  # or g.date, or index
+    
+            self.passingMaxes.append({
+                stat_name: max_value,
+                "game_dates": game_dates
+            })
 
     #   Compute maxes for all rush stats
     def set_rush_maxes(self):
@@ -71,7 +96,15 @@ class GameLog:
                 stat_name: max_value,
                 "game_dates": game_dates
             })
-
+    
+    #   Compute totals for all passing stats
+    def set_pass_totals(self):
+        for stat_name in TOTAL_PASSING_STAT_NAMES:
+            self.passingTotals[stat_name] = sum(
+                int(getattr(g, stat_name, 0) or 0)
+                for g in self.games
+            )
+            
     #   Compute totals for all rush stats
     def set_rush_totals(self):
         for stat_name in TOTAL_RUSHING_STAT_NAMES:
