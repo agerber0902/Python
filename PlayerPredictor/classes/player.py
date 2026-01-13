524b2f5e1a9232b8a50aa3ee60b717a064e69780
agerber0902 <agerber0902@gmail.com>
2026-01-12 20:41:40 -0500

    clean up print of player preformances
    create separate reusable functions for printing data

--- PlayerPredictor/classes/player.py
+++ PlayerPredictor/classes/player.py
@@ -4,54 +4,109 @@
 Created on Tue Jan  6 12:07:15 2026
 
 Main Player Class object. Inherits from PlayerInfo
+Displays all the player info and game log highlights
 
 @author: andrewgerber
 """
 from .playerInfo import PlayerInfo
+
 class Player(PlayerInfo):
     
     def __init__(self, player_info = None, game_log = None):
         #Initialize Player Info
         self.PlayerInfo = player_info
         self.GameLog = game_log
-        
-    def display(self):
-        
-        rush_att_max = self.GameLog.get_max_entry(self.GameLog.rushingMaxes, "rush_att")
-        rush_yds_max = self.GameLog.get_max_entry(self.GameLog.rushingMaxes, "rush_yds")
-        rush_td_max = self.GameLog.get_max_entry(
-            self.GameLog.rushingMaxes, "rush_td"
-        )
-        
-        rec_yds_max = self.GameLog.get_max_entry(
-            self.GameLog.receivingMaxes, "rec_yds"
-        )
-        rec_td_max = self.GameLog.get_max_entry(
-            self.GameLog.receivingMaxes, "rec_td"
-        )
-        rec_max = self.GameLog.get_max_entry(
-            self.GameLog.receivingMaxes, "rec"    
-        )
-        target_max = self.GameLog.get_max_entry(
-            self.GameLog.receivingMaxes, "targets"    
-        )
-        
+
+    def display(self):        
         return f"""
 {self.PlayerInfo.name} | {self.PlayerInfo.position}, {self.PlayerInfo.team}
 {self.PlayerInfo.height} {self.PlayerInfo.weight}lbs
+{self.display_stats(self.PlayerInfo.isQB)}
+"""
 
-Season Stats:
+    
+    def display_stats(self, isQb: bool) -> str:
+        
+        stats: str = """"""
+        totals: str = """
+Season Stats:"""
+        
+        maxes: str = """Best Preformaces:"""
+        
+        if (isQb):
+            # -- Totals
+            totals += self.display_passing_stats(True)
+            totals += "\n"
+            totals += self.display_skill_stats(True)
+            stats += totals
+            # -- Maxes
+            maxes += self.display_passing_stats(False)
+            maxes += "\n"
+            maxes += self.display_skill_stats(False)
+            stats += maxes
+        else:
+            totals += self.display_skill_stats(True)
+            stats += totals
+            stats += "\n"
+            maxes += self.display_skill_stats(False)
+            stats += maxes
+        return stats
+    
+    def display_passing_stats(self, isTotal: bool) -> str:
+        if isTotal:
+            return f"""
+        Passing
+            Attempts: {self.GameLog.passingTotals.get("pass_att", 0)}
+            Completions: {self.GameLog.passingTotals.get("pass_cmp", 0)}
+            Yards: {self.GameLog.passingTotals.get("pass_yds", 0)}
+            TD: {self.GameLog.passingTotals.get("pass_td", 0)}
+            INT: {self.GameLog.passingTotals.get("pass_int", 0)}
+        """
+        else:
+            # return max
+            pass_att_max = self.GameLog.get_max_entry(self.GameLog.passingMaxes, "pass_att")
+            pass_cmp_max = self.GameLog.get_max_entry(self.GameLog.passingMaxes, "pass_cmp")
+            pass_yds_max = self.GameLog.get_max_entry(self.GameLog.passingMaxes, "pass_yds")
+            pass_td_max = self.GameLog.get_max_entry(self.GameLog.passingMaxes, "pass_td")
+            pass_int_max = self.GameLog.get_max_entry(self.GameLog.passingMaxes, "pass_int")
+            return f"""
+        Passing
+            Attempts: {pass_att_max["pass_att"] if pass_att_max else 0}
+                Games: {self.format_dates(pass_att_max["game_dates"]) if pass_att_max else "-"}
+            Completions: {pass_cmp_max["pass_cmp"] if pass_cmp_max else 0}
+                Games: {self.format_dates(pass_cmp_max["game_dates"]) if pass_cmp_max else "-"}
+            Yards: {pass_yds_max["pass_yds"] if pass_yds_max else 0}
+                Games: {self.format_dates(pass_yds_max["game_dates"]) if pass_yds_max else "-"}
+            TD: {pass_td_max["pass_td"] if pass_td_max else 0}
+                Games: {self.format_dates(pass_td_max["game_dates"]) if pass_td_max else "-"}
+            INT: {pass_int_max["pass_int"] if pass_int_max else 0}
+                Games: {self.format_dates(pass_int_max["game_dates"]) if pass_int_max else "-"}
+        """
+        
+    def display_skill_stats(self, isTotal: bool) -> str:
+        if isTotal:
+            return f"""
     Rushing
         Attempts: {self.GameLog.rushingTotals.get("rush_att", 0)}
         Yards: {self.GameLog.rushingTotals.get("rush_yds", 0)}
         TD: {self.GameLog.rushingTotals.get("rush_td", 0)}
-
+    
     Receiving
         Catches/Targets: {self.GameLog.receivingTotals.get("rec", 0)}/{self.GameLog.receivingTotals.get("targets", 0)}
         Yards: {self.GameLog.receivingTotals.get("rec_yds", 0)}
         TD: {self.GameLog.receivingTotals.get("rec_td", 0)}
-
-Best Performances:
+        """
+        else:
+            
+            rush_att_max = self.GameLog.get_max_entry(self.GameLog.rushingMaxes, "rush_att")
+            rush_yds_max = self.GameLog.get_max_entry(self.GameLog.rushingMaxes, "rush_yds")
+            rush_td_max = self.GameLog.get_max_entry(self.GameLog.rushingMaxes, "rush_td")
+            rec_yds_max = self.GameLog.get_max_entry(self.GameLog.receivingMaxes, "rec_yds")
+            rec_td_max = self.GameLog.get_max_entry(self.GameLog.receivingMaxes, "rec_td")
+            rec_max = self.GameLog.get_max_entry(self.GameLog.receivingMaxes, "rec")
+            target_max = self.GameLog.get_max_entry(self.GameLog.receivingMaxes, "targets")
+            
+            return f"""
     Rushing
         Attempts: {rush_att_max["rush_att"] if rush_att_max else 0}
             Games: {self.format_dates(rush_att_max["game_dates"]) if rush_att_max else "-"}
@@ -73,9 +128,9 @@ Best Performances:
             Games: {self.format_dates(rec_yds_max["game_dates"]) if rec_yds_max else "-"}
 
         TDs: {rec_td_max["rec_td"] if rec_td_max else 0}
-            Games: {self.format_dates(rec_td_max["game_dates"]) if rec_td_max else "-"}
-"""
-
+            Games: {self.format_dates(rec_td_max["game_dates"]) if rec_td_max else "-"}        
+        """
+    
     def format_dates(self, dates):
         formatted = []
     
