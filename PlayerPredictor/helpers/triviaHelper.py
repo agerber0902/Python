b922f012efe6c7a11089bcdc9c6ec417a8af4bd8
agerber0902 <agerber0902@gmail.com>
2026-01-23 17:34:35 -0500

    clean up trivia helper

--- PlayerPredictor/helpers/triviaHelper.py
+++ PlayerPredictor/helpers/triviaHelper.py
@@ -17,121 +17,90 @@ from data.playerFirebase import PlayerFirebase
 from classes.playerInfo import PlayerInfo
 from classes.question import Question
 
-def generate_player_team_question(player: PlayerInfo, players: list[PlayerInfo]) -> Question:
-    player_team_question = f"What team does {player.name} play for?"
+def generate_question(player: PlayerInfo, players: list[PlayerInfo], category: str, isPlayerQuestion: bool) -> Question:
+    if category == "team":
+        question = (f"What player plays for {player.team}?" if player.team != None else "What player is a free agent?") if isPlayerQuestion else f"What team does {player.name} play for?"
+        correct = player.name if isPlayerQuestion else player.team if player.team != None else "Free Agent"
+        
+        if isPlayerQuestion:
+            wrong_answers = random.sample(
+                [p.name for p in players if p.team != player.team],
+                k=min(3, len(players) - 1)
+            )
+        else:
+            wrong_answers = random.sample(
+                [p.team if p.team != None else "Free Agent" for p in players if p.team != player.team],
+                k=min(3, len(players) - 1)
+            )
+            
+        answers = [correct] + wrong_answers
+        random.shuffle(answers)
+        return Question(question = question, answers = answers, correct = correct)
+    elif category == "position":
+        question = f"What player plays {player.position}?" if isPlayerQuestion else f"What position does {player.name} play?"
+        correct = player.name if isPlayerQuestion else player.position
+        
+        if isPlayerQuestion:
+            wrong_answers = random.sample(
+                [p.name for p in players if p.position != player.position],
+                k=min(3, len(players) - 1)
+            )
+        else:
+            wrong_answers = ["QB", "RB", "WR", "TE"]
+            
+        answers = [correct] + wrong_answers
+        random.shuffle(answers)
+        return Question(question = question, answers = answers, correct = correct)
+        
+    elif category == "college":
+        question = f"What player finished college at {player.college}" if isPlayerQuestion else f"What college did {player.name} play for last?"
+        correct = player.name if isPlayerQuestion else player.college
+        
+        if isPlayerQuestion:
+            wrong_answers = random.sample(
+                [p.name for p in players if p.college != player.college],
+                k=min(3, len(players) - 1)
+            )
+        else:
+            wrong_answers = random.sample(
+                [p.college for p in players if p.college != player.college],
+                k=min(3, len(players) - 1)
+            )
+            
+        answers = [correct] + wrong_answers
+        random.shuffle(answers)
+        return Question(question = question, answers = answers, correct = correct)
+    elif category == "draftedTeam":
+        question = (f"What player was drafted by {player.draftedTeam}" if player.draftedTeam != None else "What player went undrafted?") if isPlayerQuestion else f"What team drafted {player.name}?"
+        correct = player.name if isPlayerQuestion else player.draftedTeam if player.draftedTeam != None else "Undrafted Free Agent"
+        
+        if isPlayerQuestion:
+            wrong_answers = random.sample(
+                [p.name for p in players if p.draftedTeam != player.draftedTeam],
+                k=min(3, len(players) - 1)
+            )
+        else:
+            wrong_answers = random.sample(
+                [p.draftedTeam if p.draftedTeam != None else "Undrafted Free Agent" for p in players if p.draftedTeam != player.draftedTeam],
+                k=min(3, len(players) - 1)
+            )
+            
+        answers = [correct] + wrong_answers
+        random.shuffle(answers)
+        return Question(question = question, answers = answers, correct = correct)
+    else:
+        print(f"No category for: {category}")
+        return None
     
-    # pick 3 random players excluding the current one
-    wrong_answers = random.sample(
-        [p.team if p.team != None else "Free Agent" for p in players if p.team != player.team],
-        k=min(3, len(players) - 1)
-    )
-    correct = player.team if player.team != None else "Free Agent"
-    answers = wrong_answers + [correct]
-    random.shuffle(answers)
-    question = Question(question = player_team_question, answers = answers, correct=correct)
-    return question
-
-def generate_player_position_question(player: PlayerInfo, players: list[PlayerInfo]) -> Question:
-    player_position_question = f"What position does {player.name} play?"
-    
-    answers = ["QB", "RB", "WR", "TE"]
-    random.shuffle(answers)
-    question = Question(question = player_position_question, answers = answers, correct=player.position)
-    return question
-
-def generate_player_college_question(player: PlayerInfo, players: list[PlayerInfo]) -> Question:
-    player_college_question = f"What college did {player.name} play for last?"
-    
-    # pick 3 random players excluding the current one
-    wrong_answers = random.sample(
-        [p.college for p in players if p.college != player.college],
-        k=min(3, len(players) - 1)
-    )
-
-    answers = wrong_answers + [player.college]
-    random.shuffle(answers)
-    question = Question(question = player_college_question, answers = answers, correct=player.college)
-    return question
-
-def generate_player_drafted_team_question(player: PlayerInfo, players: list[PlayerInfo]) -> Question:
-    question = f"What team drafted {player.name}?"
-    
-    # pick 3 random players excluding the current one
-    wrong_answers = random.sample(
-        [p.draftedTeam if p.draftedTeam != None else "Undrafted" for p in players if p.draftedTeam != player.draftedTeam],
-        k=min(3, len(players) - 1)
-    )
-
-    correct = player.draftedTeam if player.draftedTeam != None else "Undrafted"
-    answers = wrong_answers + [correct]
-    random.shuffle(answers)
-    return Question(question = question, answers = answers, correct=correct)
-
-def generate_drafted_team_question(player: PlayerInfo, players: list[PlayerInfo]) -> Question:
-    question = f"What player was drafted by {player.draftedTeam}" if player.draftedTeam != None else f"What player went undrafted?"
-    
-    # pick 3 random players excluding the current one
-    wrong_answers = random.sample(
-        [p.name for p in players if p != player],
-        k=min(3, len(players) - 1)
-    )
-
-    answers = wrong_answers + [player.name]
-    random.shuffle(answers)
-    return Question(question = question, answers = answers, correct=player.name)
-
-def generate_team_question(player: PlayerInfo, players: list[PlayerInfo]) -> Question:
-    team_question = f"What player plays for {player.team}?" if player.team != None else f"What player is a free agent?"
-    
-    # pick 3 random players excluding the current one
-    wrong_answers = random.sample(
-        [p.name for p in players if p.team != player.team],
-        k=min(3, len(players) - 1)
-    )
-    
-    answers = wrong_answers + [player.name]
-    random.shuffle(answers)
-    return Question(question = team_question, answers = answers, correct=player.name)
-    
-def generate_position_question(player: PlayerInfo, players: list[PlayerInfo]) -> Question:
-    position_question = f"What player plays {player.position}?"
-    
-    # pick 3 random players excluding the current one
-    wrong_answers = random.sample(
-        [p.name for p in players if p.position != player.position],
-        k=min(3, len(players) - 1)
-    )
-    answers = wrong_answers + [player.name]
-    random.shuffle(answers)
-    return Question(question = position_question, answers = answers, correct=player.name)
-
-def generate_college_question(player: PlayerInfo, players: list[PlayerInfo]) -> Question:
-    college_question = f"What player finished college at {player.college}"
-    
-    # pick 3 random players excluding the current one
-    wrong_answers = random.sample(
-        [p.name for p in players if p.college != player.college],
-        k=min(3, len(players) - 1)
-    )
-    answers = wrong_answers + [player.name]
-    random.shuffle(answers)
-    return Question(question = college_question, answers = answers, correct=player.name)
-
 def generate_questions(players: list[PlayerInfo]) -> list[Question]:
     questions: list[Question] = []
     
     for player in players:
         n = random.choice([1, 2])
-        if n == 1:
-            questions.append(generate_team_question(player, players))
-            questions.append(generate_position_question(player, players))
-            questions.append(generate_college_question(player, players))
-            questions.append(generate_drafted_team_question(player, players))
-        else:
-            questions.append(generate_player_team_question(player, players))
-            questions.append(generate_player_position_question(player, players))
-            questions.append(generate_player_college_question(player, players))
-            questions.append(generate_player_drafted_team_question(player, players))
+        questions.append(generate_question(player, players, 'team', n == 1))
+        questions.append(generate_question(player, players, 'position', n == 1))
+        questions.append(generate_question(player, players, 'college', n == 1))
+        questions.append(generate_question(player, players, 'draftedTeam', n == 1))
     
     random.shuffle(questions)
     return questions
